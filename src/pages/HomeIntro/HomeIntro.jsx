5deed6fb6b19b3dc93740a7e97396b1c06ca30ce
nguyentran-se <nguyentranfw@gmail.com>
2021-10-14 21:21:11 +0700

    fix: intro logo

--- src/pages/HomeIntro/HomeIntro.jsx
+++ src/pages/HomeIntro/HomeIntro.jsx
@@ -20,7 +20,7 @@ const HomeIntro = () => {
       <AppBar elevation={0} position="sticky">
         <Container>
           <Toolbar sx={{ padding: { xs: 0, sm: 0, md: 0 } }}>
-            <Logo />
+            <Logo hasHref={false} />
 
             <Stack direction="row" spacing={2} sx={{ ml: 'auto' }}>
               <StyledButton
